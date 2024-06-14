7308ab459d79df0c785f35771c498f9d4f4e6a83
Julio César <jucesarcas88@gmail.com>
2024-06-14 16:27:12 -0500

    Registration form connected to backend and database

--- Frontend/src/routes/SignUp.jsx
+++ Frontend/src/routes/SignUp.jsx
@@ -16,6 +16,7 @@ export default function SignUp(){
     const[dateOfBirth, setDateOfBirth] = useState();
     const [errors, setErrors] = useState({});
     const navigate = useNavigate();
+    const isAdmin = new Boolean(true);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -61,16 +62,17 @@ export default function SignUp(){
         // If no errors, proceed with form submission
         try {
         const { data } = await axios.post(
-            "http://localhost:4000/api/v1/restaurant/send",
+            "http://localhost:4000/api/v1/registration/send",
             {
+            email,
             firstName,
             lastName,
-            email,
             password,
             phoneNumber,
             address,
             gender,
-            dateOfBirth
+            dateOfBirth,
+            isAdmin
             },
             {
             headers: {
@@ -100,49 +102,49 @@ export default function SignUp(){
     }   
 
     return (
-        <div class="register">
-        <form class="registerForm" onSubmit={handleSubmit}>
+        <div className="register">
+        <form className="registerForm" onSubmit={handleSubmit}>
 
-            <h3 class="fw-normal mb-3 pb-3 textCenter" >Register Form</h3>
+            <h3 className="fw-normal mb-3 pb-3 textCenter" >Register Form</h3>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="text" value={firstName} onChange={(e)=>setFirstName(e.target.value)} id="registerFormName" class="form-control form-control-lg" placeholder="First Name"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="text" value={firstName} onChange={(e)=>setFirstName(e.target.value)} id="registerFormName" className="form-control form-control-lg" placeholder="First Name"/>
             {errors.firstName && (<span className="error">{errors.firstName}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)} id="registerFormLastName" class="form-control form-control-lg" placeholder="Last Name"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)} id="registerFormLastName" className="form-control form-control-lg" placeholder="Last Name"/>
             {errors.lastName && (<span className="error">{errors.lastName}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} id="registerFormEmail" class="form-control form-control-lg" placeholder="Email"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} id="registerFormEmail" className="form-control form-control-lg" placeholder="Email"/>
             {errors.email && (<span className="error">{errors.email}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} id="registerFormPass" class="form-control form-control-lg" placeholder="Password"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} id="registerFormPass" className="form-control form-control-lg" placeholder="Password"/>
             {errors.password && (<span className="error">{errors.password}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="password" value={repeatPassword} onChange={(e)=>setRepeatPassword(e.target.value)} id="registerFormPass2" class="form-control form-control-lg" placeholder="Repeat Password"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="password" value={repeatPassword} onChange={(e)=>setRepeatPassword(e.target.value)} id="registerFormPass2" className="form-control form-control-lg" placeholder="Repeat Password"/>
             {errors.repeatPassword && (<span className="error">{errors.repeatPassword}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="text" value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)} id="registerFormPhone" class="form-control form-control-lg" placeholder="Phone Number"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="text" value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)} id="registerFormPhone" className="form-control form-control-lg" placeholder="Phone Number"/>
             {errors.phoneNumber && (<span className="error">{errors.phoneNumber}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} id="registerFormAddress" class="form-control form-control-lg" placeholder="Address"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} id="registerFormAddress" className="form-control form-control-lg" placeholder="Address"/>
             {errors.address && (<span className="error">{errors.address}</span>)}
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-                <select class="form-control" value={gender} onChange={(e)=>setGender(e.target.value)} id="registerFormGender">
-                    <option selected>Select Gender</option>
+            <div data-mdb-input-init className="form-outline mb-2">
+                <select className="form-control" defaultValue={'default'} value={gender} onChange={(e)=>setGender(e.target.value)} id="registerFormGender">
+                    <option value="default" disabled>Select Gender</option>
                     <option key="Male" value="Male">Male</option>
                     <option key="Female" value="Female">Female</option>
                     <option key="declineToState" value="declineToState">Decline to state</option>
@@ -150,13 +152,13 @@ export default function SignUp(){
             {errors.gender && (<span className="error">{errors.gender}</span>)}    
             </div>
 
-            <div data-mdb-input-init class="form-outline mb-2">
-            <input type="text" value={dateOfBirth} onChange={(e)=>setDateOfBirth(e.target.value)} id="registerFormDOB" class="form-control form-control-lg" placeholder="Date of Birth"/>
+            <div data-mdb-input-init className="form-outline mb-2">
+            <input type="text" value={dateOfBirth} onChange={(e)=>setDateOfBirth(e.target.value)} id="registerFormDOB" className="form-control form-control-lg" placeholder="Date of Birth"/>
             {errors.dateOfBirth && (<span className="error">{errors.dateOfBirth}</span>)}
             </div>
 
-            <div class="pt-1 mb-4">
-            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-info btn-lg btn-block" type="submit">Register</button>
+            <div className="pt-1 mb-4">
+            <button data-mdb-button-init data-mdb-ripple-init className="btn btn-info btn-lg btn-block" type="submit">Register</button>
             </div>
 
         </form>
